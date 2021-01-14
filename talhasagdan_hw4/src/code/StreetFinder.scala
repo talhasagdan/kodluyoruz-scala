af75a17d3823e008559a4acfcda024d98756d81f
Talha Sagdan <43733194+talhasagdan@users.noreply.github.com>
2021-01-14 12:48:29 +0300

    Add files via upload

---
+++ talhasagdan_hw4/src/code/StreetFinder.scala
@@ -0,0 +1,11 @@
+package code
+
+trait StreetFinder {
+  //: List[String]
+  def findStreets(streets: List[String], names: Set[String])={
+    for (i <- names){
+      println(Console.YELLOW +s"Number of $i named street : ${streets.filter(x => x contains i).length}")
+      println(Console.WHITE+ streets.filter(x => x contains i))
+    }
+  }
+}
